11e1cd95bb29bc6915e89ef7de6ea629b9190d2e
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-08-28 18:22:32 -0300

    add Button and Display to Calculator component

---
+++ src/main/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Display from "../components/Display";
+import Button from "../components/Button";
+
+export default {
+  components: { Button, Display },
+};
+</script>
+
+<style>
+.calculator {
+  width: 235px;
+  height: 320px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
